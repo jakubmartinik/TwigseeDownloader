c07209115da09a8ed59c337737fa01da339667c9
Jakub Martiník <email@jakubmartinik.cz>
2026-04-20 00:33:17 +0200

    Change to python #patch

--- rohlik_mcp/run.sh
+++ rohlik_mcp/run.sh
@@ -5,11 +5,13 @@ USERNAME=$(jq -r '.username' /data/options.json)
 PASSWORD=$(jq -r '.password' /data/options.json)
 BASE_URL=$(jq -r '.base_url' /data/options.json)
 
-echo "DEBUG: Username=$ROHLIK_USERNAME"
-echo "DEBUG: Base=$ROHLIK_BASE_URL"
 
 export ROHLIK_USERNAME="$USERNAME"
 export ROHLIK_PASSWORD="$PASSWORD"
 export ROHLIK_BASE_URL="$BASE_URL"
 
+
+echo "DEBUG: Username=$ROHLIK_USERNAME"
+echo "DEBUG: Base=$ROHLIK_BASE_URL"
+echo "DEBUG: options.json=$(cat /data/options.json)"
 exec mcp-proxy --host 0.0.0.0 --port 8811 -- npx @tomaspavlin/rohlik-mcp
